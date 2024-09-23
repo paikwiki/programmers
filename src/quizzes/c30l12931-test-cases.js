a5d35e5064ff159fb81a2b344abc4304b3e35fcf
paikwiki <paikwiki@gmail.com>
2024-09-23 21:22:12 +0900

    :sparkles: Done c30l12931

---
+++ src/quizzes/c30l12931-test-cases.js
@@ -0,0 +1,10 @@
+export const testCases = [
+  {
+    input: [123],
+    output: 6,
+  },
+  {
+    input: [987],
+    output: 24,
+  },
+];
---
+++ src/quizzes/c30l12931.js
@@ -0,0 +1,3 @@
+export function solution(n) {
+  return `${n}`.split("").reduce((acc, curr) => acc + parseInt(curr), 0);
+}
