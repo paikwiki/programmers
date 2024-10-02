25a795bbbee6850c7b4f62d83550484e13622d76
paikwiki <paikwiki@gmail.com>
2024-10-02 23:32:33 +0900

    :sparkles: Done c30l12918

---
+++ src/quizzes/c30l12918-test-cases.js
@@ -0,0 +1,4 @@
+export const testCases = [
+  { input: ["a234"], output: false },
+  { input: ["1234"], output: true },
+];
---
+++ src/quizzes/c30l12918.js
@@ -0,0 +1,13 @@
+export function solution(s) {
+  const numChars = "1234567890";
+  if (![4, 6].includes(s.length)) return false;
+
+  for (let index = 0; index < s.length; index++) {
+    if (!numChars.includes(s[index])) {
+      console.log(numChars.includes(s[index]));
+      return false;
+    }
+  }
+
+  return true;
+}
